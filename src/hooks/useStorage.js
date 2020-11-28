d24fecc1cbe32288a4ef44f0ebee1069bc18fbcd
shuvamk <kshuvam564@gmail.com>
2020-11-27 21:14:09 -0800

    Created custom hook to upload image

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { projectStorage } from "../firebase/config";
+
+const useStorage = (profileImage) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = projectStorage.ref(profileImage.name);
+    storageRef.put(profileImage).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadUrl();
+        setUrl(url);
+      }
+    );
+  }, [profileImage]);
+  return { progress, url, error };
+};
+export default useStorage;
