f457950ad1d224e09fedcf863645ee6d5555ac38
shuvamk <kshuvam564@gmail.com>
2020-11-27 23:18:51 -0800

    Successfully fetching data based on query

--- src/components/PageDisplay.js
+++ src/components/PageDisplay.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import useFirestore from "../hooks/useFirestore";
 
 export default function PageDisplay() {
   const { users } = useFirestore("users");
+  const [userQuery, setUserQuery] = useState();
+  const [displayUser, setDisplayUser] = useState();
+  const handleChange = (e) => {
+    setUserQuery(e.target.value);
+  };
+  const handleClick = () => {
+    let temp = users.filter((user) =>
+      user.username.username.includes(userQuery)
+    );
+    setDisplayUser(temp);
+  };
   console.log(users);
-  return <div></div>;
+  console.log(displayUser);
+  return (
+    <div>
+      <h1>Hello Frans</h1>
+      <input type="text" placeholder="Enter Username" onChange={handleChange} />
+      <button onClick={handleClick}>Find</button>
+      {displayUser && <div>{displayUser[0].name.name}</div>}
+    </div>
+  );
 }
