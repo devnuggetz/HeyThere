f8d0c95e2a493dcf6d897cff83e7660741a809d6
shuvamk <kshuvam564@gmail.com>
2020-11-27 22:05:16 -0800

    Added the progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import useStorage from "../hooks/useStorage";
+import "./ProgressBar.css";
 
 export default function ProgressBar({ profileImage, setProfileImage }) {
   const { url, progress } = useStorage(profileImage);
   console.log(progress, url);
-  return <div className="progressBar">Progress</div>;
+  return <div className="progressBar" style={{ width: progress + "%" }}></div>;
 }
