a8a6f6ce543ffa39b4815903c8c4fc48347ec31c
shuvamk <kshuvam564@gmail.com>
2020-11-28 12:47:27 -0800

    Dynamic rednering on card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,9 +3,17 @@ import "./UserCard.css";
 
 function UserCard({ displayUser }) {
   const [imageUrl, setImageUrl] = useState();
+  const [name, setnName] = useState();
+  const [bio, setBio] = useState();
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
   useEffect(() => {
     if (displayUser) {
       setImageUrl(displayUser[0].url);
+      setBio(displayUser[0].bio.bio);
+      setnName(displayUser[0].name.name);
+      setEmail(displayUser[0].email.email);
+      setUsername(displayUser[0].username.username);
     }
   }, []);
   console.log(displayUser);
@@ -17,11 +25,11 @@ function UserCard({ displayUser }) {
           <img for="photo-upload" src={imageUrl} />
         </div>
       </label>
-      <h2>Shuvam Kumar</h2>
-      <p>Username</p>
-      <p>Email</p>
+      <h2>{name}</h2>
+      <p>{username}</p>
+      <p>{email}</p>
       <div className="userCard__bio">
-        <p>bio</p>
+        <p>{bio}</p>
       </div>
     </div>
   );
